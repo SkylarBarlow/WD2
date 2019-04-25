2e5e49ce6945c1e42901513b56b3652e79428faf
SkylarBarlow <47986644+SkylarBarlow@users.noreply.github.com>
2019-04-25 12:28:05 -0400

    Update code.js

--- code.js
+++ code.js
@@ -2,4 +2,16 @@
       $('.toggleNav').click(function() {
         $('#flex-nav ul').toggleClass('open');
       });
+  $('.call').click(function() {
+   $('.phone').slideToggle('slow');
+  });
+ $('.button').click(
+    function(){
+     $(this).removeClass('shadow')
+   });
+  $('.button').mouseleave(
+     function(){
+     $(this).addClass('shadow');
+     });
     });
+
